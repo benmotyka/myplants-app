143b79e480cc5becd45f800b894717e721529170
ben <benmotykax@gmail.com>
2022-06-11 00:02:15 +0200

    added basic logic for plant component

--- components/Plant/Plant.tsx
+++ components/Plant/Plant.tsx
@@ -3,29 +3,44 @@ import { Slider } from "@miblanchard/react-native-slider";
 
 import { Body, Header, Image, Wrapper, Container } from "./Plant.styles";
 import { PlantProps } from "./Plant.interface";
+import { Text } from "react-native";
 
-const MAX_SLIDER_VALUE = 1
+const MAX_SLIDER_VALUE = 1;
+const SLIDE_SUCCESS_THRESHOLD = 0.9;
 
 const Plant = ({ name, imgSrc }: PlantProps) => {
   const [sliderValue, setSliderValue] = React.useState(0);
+  const [watered, setWatered] = React.useState(false);
 
-  const submitPlant = (value: number | number[]) => {
-    console.log(MAX_SLIDER_VALUE)
-    // if value > 0.9 then hide
-  }
+  const submitPlant = (value: number | number[]): void => {
+    const currentValue = typeof value !== "number" ? value[0] : value;
+    if (currentValue >= SLIDE_SUCCESS_THRESHOLD * MAX_SLIDER_VALUE) {
+      setWatered(true);
+    }
+  };
+
   return (
     <Container>
       <Wrapper>
         <Image resizeMode="cover" source={imgSrc} />
         <Body>
           <Header>{name}</Header>
-          <Slider
-            value={sliderValue}
-            onSlidingComplete={submitPlant}
-            thumbStyle={{backgroundColor: '#444444', borderRadius: 5, width: 35, height: 25}}
-            trackStyle={{opacity: 0.3}}
-            maximumValue={MAX_SLIDER_VALUE}
-          />
+          {watered ? (
+            <Text>sukces</Text>
+          ) : (
+            <Slider
+              value={sliderValue}
+              onSlidingComplete={submitPlant}
+              thumbStyle={{
+                backgroundColor: "#444444",
+                borderRadius: 5,
+                width: 35,
+                height: 25,
+              }}
+              trackStyle={{ opacity: 0.3 }}
+              maximumValue={MAX_SLIDER_VALUE}
+            />
+          )}
         </Body>
       </Wrapper>
     </Container>
