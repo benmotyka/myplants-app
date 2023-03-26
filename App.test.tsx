3a63652350501c78ad6ccd5e91daf8fab530d4c6
benmotyka <benmotykax@gmail.com>
2023-03-26 23:54:05 +0200

    test: change config

---
+++ App.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App from './App';
+
+describe('<App />', () => {
+  it('has 1 child', () => {
+    const tree = renderer.create(<App />).toJSON();
+    // @ts-ignore
+    expect(tree.children.length).toBe(1);
+  });
+});
