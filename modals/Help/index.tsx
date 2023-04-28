699f0977a4e13080f712ce4de5e358a44732a51c
benmotyka <benmotykax@gmail.com>
2023-04-28 23:14:07 +0200

    add fancy animations for help modal

--- modals/Help/index.tsx
+++ modals/Help/index.tsx
@@ -1,16 +1,41 @@
-import React, { ReactElement, useEffect, useState } from "react";
+import React, { ReactElement, useEffect, useRef, useState } from "react";
+import { View } from "react-native";
 import { useModalsStore } from "store/index";
+import { MotiView } from "moti";
 import { ModalAnimationWrapper } from "styles/shared";
 import { useTheme } from "styled-components/native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { ScreenOverlay, PlantContainer, SliderContainer, Text } from "./styles";
+import {
+    ScreenOverlay,
+    PlantContainer,
+    SliderContainer,
+    Text,
+    TextWrapper,
+} from "./styles";
 
-const ICON_SIZE_PX = 52;
+const TAP_ICON_SIZE_PX = 60;
+
+const TapIcon = ({ paddingTop }: { paddingTop?: number }): JSX.Element => {
+    const theme = useTheme();
+
+    return (
+        <MaterialCommunityIcons
+            name="gesture-tap"
+            size={TAP_ICON_SIZE_PX}
+            color={theme.white}
+            style={{
+                paddingTop,
+            }}
+        />
+    );
+};
 
 const HelpModal = (): JSX.Element => {
     const [step, setStep] = useState(0);
+    const [sliderWidth, setSliderWidth] = useState(0);
+    const ref = useRef<View>(null);
+
     const modalsStore = useModalsStore((state) => state);
-    const theme = useTheme();
 
     useEffect(() => {
         if (step < 3) return;
@@ -19,30 +44,72 @@ const HelpModal = (): JSX.Element => {
 
     const slides: ReactElement[] = [
         <>
-            <SliderContainer></SliderContainer>
-            <Text>Move the slider to mark your plant as watered</Text>
+            <SliderContainer
+                ref={ref}
+                onLayout={() =>
+                    ref.current?.measure((x, y, width) => setSliderWidth(width))
+                }
+            >
+                <MotiView
+                    animate={{
+                        translateX: [
+                            {
+                                value: sliderWidth - TAP_ICON_SIZE_PX,
+                                delay: 1000,
+                                damping: 100,
+                            },
+                            { value: 2, delay: 1000, damping: 100 },
+                        ],
+                    }}
+                    transition={{
+                        loop: true,
+                    }}
+                >
+                    <TapIcon paddingTop={15} />
+                </MotiView>
+            </SliderContainer>
+            <TextWrapper>
+                <Text>Move the slider to mark your plant as watered</Text>
+            </TextWrapper>
         </>,
         <>
             <PlantContainer>
-                <MaterialCommunityIcons
-                    name="gesture-tap"
-                    size={ICON_SIZE_PX}
-                    color={theme.white}
-                />
+                <MotiView
+                    animate={{
+                        scale: [{ value: 0.8, delay: 1000 }, 1],
+                    }}
+                    transition={{
+                        loop: true,
+                    }}
+                >
+                    <TapIcon />
+                </MotiView>
             </PlantContainer>
-            <Text>
-                Tap your plant to see the watering history or add images
-            </Text>
+            <TextWrapper>
+                <Text>
+                    Tap your plant to see the watering history or add images
+                </Text>
+            </TextWrapper>
         </>,
         <>
             <PlantContainer>
-                <MaterialCommunityIcons
-                    name="gesture-double-tap"
-                    size={ICON_SIZE_PX}
-                    color={theme.white}
-                />
+                <MotiView
+                    animate={{
+                        scale: [
+                            { value: 0.8, delay: 1000 },
+                            { value: 1, delay: 1000 },
+                        ],
+                    }}
+                    transition={{
+                        loop: true,
+                    }}
+                >
+                    <TapIcon />
+                </MotiView>
             </PlantContainer>
-            <Text>Hold it to edit its details</Text>
+            <TextWrapper>
+                <Text>Hold it to edit its details</Text>
+            </TextWrapper>
         </>,
     ];
 
--- modals/Help/styles.ts
+++ modals/Help/styles.ts
@@ -10,22 +10,25 @@ export const ScreenOverlay = styled(DarkScreenOverlay)`
 export const PlantContainer = styled.View`
     width: ${plantWidthPercentage};
     height: 250px;
-    padding: 5px;
-    border: ${({ theme }) => `3px solid ${theme.primaryLight}`};
-    border-radius: 20px;
     display: flex;
     align-items: center;
     justify-content: center;
 `;
 
-export const SliderContainer = styled(PlantContainer)`
-    margin-top: 195px;
-    height: 45px;
+export const SliderContainer = styled.View`
+    width: ${plantWidthPercentage};
+    margin-top: 180px;
+`;
+
+export const TextWrapper = styled.View`
+    display: flex;
+    width: 100%;
 `;
 
 export const Text = styled.Text`
     font-family: "AkayaKanadaka_400Regular";
     font-size: 26px;
     color: ${({ theme }) => theme.white};
-    padding: 20px;
+    padding: 10px;
+    max-width: 50%;
 `;
