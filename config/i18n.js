3832ac717a2796fffd03e39a3cce0960e7573e69
benmotyka <benmotykax@gmail.com>
2023-04-30 00:16:35 +0200

    add de translation

--- config/i18n.js
+++ config/i18n.js
@@ -2,10 +2,12 @@ import * as Localization from "expo-localization";
 import i18n from "i18n-js";
 import en from "../translations/en.json";
 import pl from "../translations/pl.json";
+import de from "../translations/de.json";
 
 i18n.translations = {
     en,
     pl,
+    de,
 };
 
 i18n.locale = Localization.locale;
