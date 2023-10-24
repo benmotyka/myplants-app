b05632c9471877687714fb011673f517dbc9e2ef
benmotyka <benmotykax@gmail.com>
2023-10-24 19:51:37 +0200

    add workaround for some ios devices

--- config/api/plants.ts
+++ config/api/plants.ts
@@ -3,18 +3,23 @@ import { apiUrl } from "config/environment";
 import { getDeviceId } from "utils/device";
 
 const plantsApi = axios.create({
-    baseURL: apiUrl,
-    timeout: 10000,
+  baseURL: apiUrl,
+  timeout: 10000,
 });
 
 plantsApi.interceptors.request.use(async (config) => {
-    if (config.headers) config.headers["device-id"] = await getDeviceId();
-    return config;
+  const deviceId = await getDeviceId();
+
+  if (config.headers) {
+    // Workaround for some ios devices
+    config.headers["device-id"] = deviceId.replace(/"/g, '');
+  }
+  return config;
 });
 
 plantsApi.interceptors.response.use(
-    (response) => response,
-    (error) => Promise.reject(error.response?.data?.message)
+  (response) => response,
+  (error) => Promise.reject(error.response?.data?.message)
 );
 
 export default plantsApi;
